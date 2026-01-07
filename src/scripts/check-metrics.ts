7b35b842e59f2669d7e9a6a8cc31fd352c6263b8
Abdul Rahman <mrrehman002@gmail.com>
2026-01-07 16:01:07 +0500

    ready to deploy

--- src/scripts/check-metrics.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-async function main() {
-  const metrics = await prisma.dailyMetrics.findMany({
-    take: 5,
-    orderBy: { date: "desc" },
-  });
-}
-main().finally(() => prisma.$disconnect());
--- src/scripts/list-methods.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-async function main() {
-  const methods = await prisma.storePaymentMethod.findMany();
-  console.log(JSON.stringify(methods, null, 2));
-}
-main().finally(() => prisma.$disconnect());
